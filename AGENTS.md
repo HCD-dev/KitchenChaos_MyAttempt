5b66983bc6efccbb2bf87c901b83ffe68ebfacd1
Hüseyin Çağatay DOĞRUYOL <61147184+HCD-dev@users.noreply.github.com>
2026-02-06 13:08:00 +0300

    Player movement added

---
+++ AGENTS.md
@@ -0,0 +1,8 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: KitchenChaos_MyAttempt
+- Unity version: Unity 6000.3.4f1
+- Active game object:
+  - Name: Capsule
+  - Tag: Untagged
+  - Layer: Default
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float moveSpeed = 7f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,30 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 inputVector = new Vector2(0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y += 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y -= 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x -= 1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x += 1;
+        }
+
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y).normalized;
+        Vector3 moveAmount = moveDir * moveSpeed * Time.deltaTime;
+
+        // Hareketi uyguluyoruz
+        transform.Translate(moveAmount, Space.World);
     }
 }
